6fcd4e3d223ee8fdcadeaf7d2f3868dfd3a2191e
Jason <jhu7235@github.com>
2017-07-11 22:42:22 -0500

    increase player # font size

--- app/src/main/java/com/game/jason/chardeemacdennis20170417/NumberPicker.java
+++ app/src/main/java/com/game/jason/chardeemacdennis20170417/NumberPicker.java
@@ -36,7 +36,7 @@ public class NumberPicker extends android.widget.NumberPicker {
 
     private void updateView(View view) {
         if(view instanceof EditText){
-            ((EditText) view).setTextSize(20);
+            ((EditText) view).setTextSize(27);
             ((EditText) view).setTextColor(Color.parseColor("#000000"));
         }
     }
